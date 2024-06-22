1d5738d3c6e31de96e8f440c272de5e82ef4a1f4
lzxdjb <lzxdjb@berkeley.edu>
2024-06-22 21:17:38 +0800

    asdasdf

---
+++ FinalCudaParallel/src/head.cu
@@ -0,0 +1,229 @@
+#include "head.cuh"
+
+void debug(TinyCache *solvergpu)
+{
+    for (int i = 0; i < horizon; i++)
+    {
+        // std::cout << "state_vectors = " << solvergpu[i].state1 << std::endl;
+        // std::cout << "control_vectors = " << solvergpu[i].control << std::endl;
+
+        // std::cout << "gradient = " << solvergpu[i].gradient << std::endl;
+        // std::cout << "Hessian = " << solvergpu[i].Hessian << std::endl;
+
+        // std::cout << "equality = " << solvergpu[i].equality << std::endl;
+
+        // std::cout << "JB1 = " << solvergpu[i].JB1 << std::endl;
+
+        // std::cout << "JB2 = " << solvergpu[i].JB2 << std::endl;
+
+        std::cout << "LowerLeftDown = \n" << solvergpu[i].LowerLeftDown1 << std::endl;
+
+        std::cout << "LowerLeftDown = \n" << solvergpu[i].LowerLeftDown2 << std::endl;
+       
+
+        // std::cout << "final_state = " << solvergpu[i].final_state << std::endl;
+        // std::cout << "initstate = " << solvergpu[i].initial_state << std::endl;
+
+        // std::cout << "FinalMatrix = \n"
+        //           << solvergpu[i].OriginalMatrix << std::endl;
+        // std::cout << "FinalColumn = \n"
+        //           << solvergpu[i].FinalColumn << std::endl;
+
+        // std::cout << "L = \n" << solvergpu[i].L << std::endl;
+
+        // std::cout << "varible1 = \n" << solvergpu[i].varible1 << std::endl;
+        // std::cout << "varible2 = \n"
+        //           << solvergpu[i].varible2 << std::endl;
+    }
+}
+
+__global__ void solve_kernel(TinyCache *solver_gpu)
+{
+
+    int idx = threadIdx.x;
+    if (idx < horizon)
+    {
+       __shared__ SharedMatrix shared;
+
+
+        for(int i = 0 ; i < horizon * StateShape ; i++)
+        {
+            for(int j = 0 ; j < horizon ; j ++)
+            {
+                shared.row(i)[j] = 0;
+            }
+        }
+
+        __syncthreads();
+
+        
+        QCost Q = solver_gpu[idx].Q;
+        RCost R = solver_gpu[idx].R;
+
+        gradient Allgradient;
+        Hessian Hessian;
+        equality equality;
+
+        JB JB1;
+        JB JB2;
+
+        FinalMatrix finalMatrix;
+        FinalColumn finalColumn;
+
+        varible varible1;
+        varible varible2;
+
+        FinalMatrix L;
+        // solve2();
+
+        double learning_rate = 1;
+
+        tinytype norm;
+
+        JB LowerLeftDown1;
+        JB LowerLeftDown2;
+
+        LowerLeftDown1.setZero();
+        LowerLeftDown2.setZero();
+
+        // while (1)
+
+        for (int i = 0; i < 1; i++)
+        {
+
+            Hessian = GetHessian(Q, R);
+            equality = GetEquality(solver_gpu[idx].state1, solver_gpu[idx].control, solver_gpu[idx].initial_state);
+
+            if (idx != horizon - 1)
+            {                             
+                //  printf("asdfasd");
+
+                if (idx == 0)
+                {
+                    JB1 = GetJB1(solver_gpu[idx].state1 , solver_gpu[idx].initial_state);
+                    JB2 = GetJB2(solver_gpu[idx].state1, solver_gpu[idx + 1].control);
+                }
+                else
+                {
+                    JB1 = GetJB1(solver_gpu[idx].state1, solver_gpu[idx - 1].state1);
+                    JB2 = GetJB2(solver_gpu[idx].state1, solver_gpu[idx + 1].control);
+                }
+
+               
+            }
+
+            else
+            {
+            // printf("kkkkkkk");
+
+                JB1 = GetJB1(solver_gpu[idx].state1, solver_gpu[idx - 1].state1);
+                JB2.setZero();
+            }
+
+            Allgradient = GetGradient(solver_gpu[idx].state1, solver_gpu[idx].control, solver_gpu[idx].final_state, Q, R);
+
+            LowerLeftDown1 = JB1.lazyProduct(PsedoInverse(Hessian));
+            
+            LowerLeftDown2 = JB2.lazyProduct(PsedoInverse(Hessian));
+
+            temp temp1;
+            temp1 = - LowerLeftDown1.lazyProduct(JB1.transpose());
+
+            mycopy(&shared , temp1 , idx);
+            
+         
+
+
+            // finalMatrix = GetFinalMatrix(Hessian, JB);
+
+            // finalColumn = GetFinalColumn(Allgradient, equality);
+
+            // solver_gpu[idx].OriginalMatrix = finalMatrix;
+            // solver_gpu[idx].OriginalColumn = finalColumn;
+
+            // L = FinalMatrix::Identity(finalMatrix.rows(), finalMatrix.cols());
+
+            // RowElimination(&finalMatrix, &L, 0, 0, 5, 0);
+            // RowElimination(&finalMatrix, &L, 1, 1, 6, 1);
+            // RowElimination(&finalMatrix, &L, 2, 2, 7, 2);
+            // RowElimination(&finalMatrix, &L, 4, 4, 7, 4);
+            // // FinalMatrix tem =  finalMatrix;
+
+            // RowElimination(&finalMatrix, &L, 3, 3, 5, 3);
+            // RowElimination(&finalMatrix, &L, 3, 3, 6, 3);
+            // RowElimination(&finalMatrix, &L, 5, 5, 6, 5);
+
+            // solve1(&L, &finalMatrix, &varible1, &varible2, &finalColumn);
+            // solver_gpu[idx].state1.segment(0, StateShape) += learning_rate * varible2.segment(0, StateShape);
+
+            // solver_gpu[idx].control.segment(0, ControlShape) += learning_rate * varible2.segment(StateShape, ControlShape);
+
+            // // if (pivot == idx)
+            // // {
+            // //     norm = 10;
+            // // }
+            // // else
+
+            // norm = varible2.segment(0, ControlShape + StateShape).norm();
+            // // }
+
+            // printf("norm %f \n", norm);
+            // printf("pivot =  %d\n", pivot);
+            // printf("idx %d \n", idx);
+
+            // if (norm < 1e-8 && horizon - 1 == idx)
+            // {
+
+            //     printf("@@@@@@@@@@@@@@ idx = %d , pivot = %d \n", idx, pivot);
+            //     for (int i = pivot + 1; i < horizon; i++)
+            //     {
+
+            //         solver_gpu[i].initial_state.segment(0 , StateShape) = solver_gpu[pivot].state1.segment(0 , StateShape);
+
+            //     }
+
+            //     pivot++;
+            // }
+            // __syncthreads();
+
+            // // atomicAdd(&pivot, 1);  // Only one thread in the warp updates the pivot
+            // if (pivot - 1 == idx)
+            // {
+            //     printf("jbjbjbjbjbjbj pivot = %d , idx = %d\n", pivot, idx);
+
+            //     break;
+            // }
+        }
+
+        solver_gpu[idx].equality = equality;
+        solver_gpu[idx].JB1 = JB1;
+        solver_gpu[idx].JB2 = JB2;
+        solver_gpu[idx].Hessian = Hessian;
+        solver_gpu[idx].gradient = Allgradient;
+        solver_gpu[idx].FinalMatrix = finalMatrix;
+        solver_gpu[idx].FinalColumn = finalColumn;
+
+        solver_gpu[idx].L = L;
+        solver_gpu[idx].varible1 = varible1;
+        solver_gpu[idx].varible2 = varible2;
+        solver_gpu[idx].LowerLeftDown1 = LowerLeftDown1; 
+
+        solver_gpu[idx].LowerLeftDown2 = LowerLeftDown2;    
+   
+    }
+}
+
+void tiny_solve_cuda(TinyCache *cache)
+{
+    TinyCache *solver_gpu;
+
+    // debug(cache);
+    checkCudaErrors(cudaMalloc((void **)&solver_gpu, sizeof(TinyCache) * horizon));
+    checkCudaErrors(cudaMemcpy(solver_gpu, cache, sizeof(TinyCache) * horizon, cudaMemcpyHostToDevice));
+
+    solve_kernel<<<1, horizon>>>(solver_gpu);
+    checkCudaErrors(cudaDeviceSynchronize());
+
+    checkCudaErrors(cudaMemcpy(cache, solver_gpu, sizeof(TinyCache) * horizon, cudaMemcpyDeviceToHost));
+    debug(cache);
+}
---
+++ FinalCudaParallel/src/head.cuh
@@ -0,0 +1,151 @@
+#pragma once
+
+// #include <thrust/host_vector.h>
+// #include <thrust/device_vector.h>
+#include <iostream>
+#include <vector>
+#include <Eigen/Dense>
+#include "type.cuh"
+
+using std::vector;
+using namespace Eigen;
+
+#define checkCudaErrors(x) check((x), #x, __FILE__, __LINE__)
+
+template <typename T>
+void check(T result, char const *const func, const char *const file, int const line)
+{
+    if (result)
+    {
+        fprintf(stderr, "CUDA error at %s:%d code=%d(%s) \"%s\" \n", file, line,
+                static_cast<unsigned int>(result), cudaGetErrorName(result), func);
+    }
+}
+
+__global__ void solve_kernel(TinyCache *solver_gpu);
+void tiny_solve_cuda(TinyCache *cache);
+
+__device__ static inline double dot_product(double *a, double *b, int number)
+{
+    double result = 0;
+    for (int i = 0; i < number; i++)
+    {
+        result += a[i] * b[i];
+    }
+    return result;
+}
+
+__device__ static inline void copy(double *a, gradient b, int number)
+{
+    double result = 0;
+    for (int i = 0; i < number; i++)
+    {
+        a[i] = b[i];
+    }
+}
+
+__device__ static inline gradient GetGradient(state state1, control control, final_state final_state, QCost Q, RCost R)
+{
+
+    Eigen::VectorXd Q_result;
+
+    Eigen::VectorXd tempj = (state1 - final_state);
+
+    Q_result = 2 * Q.lazyProduct(tempj);
+
+    Eigen::VectorXd R_result;
+    R_result = 2 * R.lazyProduct(control);
+
+    Matrix<double, ControlShape + StateShape, 1> temp;
+
+    temp.topLeftCorner(StateShape, 1) = Q_result;
+    temp.bottomRightCorner(ControlShape, 1) = R_result;
+
+    return temp;
+}
+
+__device__ static inline Hessian GetHessian(QCost Q, RCost R)
+{
+    int totalSize = StateShape + ControlShape;
+    MatrixXd M = MatrixXd::Zero(totalSize, totalSize);
+
+    // Place Q in the top-left corner
+    M.topLeftCorner(StateShape, StateShape) = Q;
+
+    // Place R in the bottom-right corner
+    M.bottomRightCorner(ControlShape, ControlShape) = R;
+    return M * 2;
+}
+
+__device__ static inline equality GetEquality(state state1, control control, state initial)
+{
+    equality temp;
+    temp[0] = (state1[0] - initial[0] - cos(initial[2]) * control[0]) * T;
+    temp[1] = (state1[1] - initial[1] - sin(initial[2]) * control[0]) * T;
+    temp[2] = (state1[2] - initial[2] - control[1]) * T;
+
+    return temp;
+}
+
+__device__ static inline JB GetJB1(state state1, state initial)
+{
+    StateJB StateJB;
+    StateJB << 1, 0, 0,
+        0, 1, 0,
+        0, 0, 1;
+    ControlJB ControlJB;
+    ControlJB << -cos(initial[2]), 0,
+        -sin(initial[2]), 0,
+        0, -1;
+    JB JB;
+    JB.topLeftCorner(StateShape, StateShape) = StateJB;
+    JB.bottomRightCorner(StateShape, ControlShape) = ControlJB;
+
+    return JB;
+}
+
+__device__ static inline JB GetJB2(state state1, control control)
+{
+    StateJB StateJB;
+    StateJB << -1, 0, control[0] * sin(state1[2]),
+        0, -1, control[0] * cos(state1[2]),
+        0, 0, -1;
+    ControlJB ControlJB;
+    ControlJB << 0, 0,
+        0, 0,
+        0, 0;
+    JB JB;
+    JB.topLeftCorner(StateShape, StateShape) = StateJB;
+    JB.bottomRightCorner(StateShape, ControlShape) = ControlJB;
+
+    return JB;
+}
+
+__device__ static inline Hessian PsedoInverse(Hessian hessian)
+{
+    Hessian temp;
+    temp.setZero();
+    for (int i = 0; i < StateShape + ControlShape; i++)
+    {
+        temp.row(i)[i] = 1 / hessian.row(i)[i];
+    }
+    return temp;
+}
+
+__device__ static inline void mycopy(SharedMatrix *shared, temp temp1, int idx)
+{
+    int base = idx * StateShape;
+
+    for (int i = 0; i < StateShape; i++)
+    {
+        for (int j = 0; j < StateShape; j++)
+        {
+            atomicAdd(&shared->row(i + base)[j + base], temp1.row(i)[j]);
+
+            if (idx != horizon - 1)
+            {
+                 atomicAdd(&shared->row(i + base)[j + base], temp1.row(i)[j]);
+            }
+        }
+    }
+}
